22a60cf177fa98908460bfb5324b382724f941e5
Cristian Manolache <cristianmanolache0@gmail.com>
2022-03-15 00:57:39 +0200

    Create Project - First project where I use Spring Security

---
+++ src/main/java/com/lov2code/SpringSecurityAmigoscodeApplication.java
@@ -0,0 +1,17 @@
+package com.lov2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringSecurityAmigoscodeApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringSecurityAmigoscodeApplication.class, args);
+        for (String s : context.getBeanDefinitionNames()) {
+            System.out.println(s);
+        }
+    }
+
+}
