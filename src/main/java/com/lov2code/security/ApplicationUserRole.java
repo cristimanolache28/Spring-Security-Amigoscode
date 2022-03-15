e09a8f866cec15eca73948877d5d866b2eac4e5c
Cristian Manolache <cristianmanolache0@gmail.com>
2022-03-15 14:09:21 +0200

    add rules and permissions using enums

---
+++ src/main/java/com/lov2code/security/ApplicationUserRole.java
@@ -0,0 +1,22 @@
+package com.lov2code.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.lov2code.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
